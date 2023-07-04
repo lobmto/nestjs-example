32bdc2b293d142c7667533ec057cb265137d0640
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-07-04 13:34:58 +0900

    feat: Add test of RecoredsRepository

---
+++ src/records/records.repository.spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RecordsService } from './records.service';
+import { RecordsRepository } from './records.repository';
+import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoModule } from 'src/dynamo/dynamo.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvironmentVariables, validate } from 'src/env';
+
+describe('RecordsRepository', () => {
+  let dbClient: DynamoDBDocumentClient;
+  let repository: RecordsRepository;
+
+  beforeEach(async () => {
+    dbClient = jest.requireMock('src/dynamo/dynamo.module');
+    repository = new RecordsRepository(dbClient);
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  describe('findOne()', () => {
+    it('should find a record by id', async () => {
+      dbClient.send = jest
+        .fn()
+        .mockImplementation(async (command: GetCommand) => {
+          expect(command.input.TableName).toBe('test');
+          expect(command.input.Key).toStrictEqual({ id: '1234' });
+          return {
+            Item: {
+              message: 'This is test',
+              id: '1234',
+            },
+          };
+        });
+
+      const res = await repository.findOne('1234');
+      expect(res).toStrictEqual({
+        id: '1234',
+        message: 'This is test',
+      });
+    });
+
+    it('should return null when not existed id', async () => {
+      dbClient.send = jest
+        .fn()
+        .mockImplementation(async (command: GetCommand) => {
+          expect(command.input.TableName).toBe('test');
+          expect(command.input.Key).toStrictEqual({ id: '1234' });
+          return {
+            Item: undefined,
+          };
+        });
+
+      const res = await repository.findOne('1234');
+      expect(res).toBe(null);
+    });
+  });
+});
--- src/records/records.repository.ts
+++ src/records/records.repository.ts
@@ -6,14 +6,16 @@ export class RecordsRepository {
   constructor(
     private readonly dynamoDBDocumentClient: DynamoDBDocumentClient,
   ) {}
-  async findOne(id: string) {
+  async findOne(id: string): Promise<Record<string, any> | null> {
     return (
-      await this.dynamoDBDocumentClient.send(
-        new GetCommand({
-          TableName: 'test',
-          Key: { id },
-        }),
-      )
-    ).Item;
+      (
+        await this.dynamoDBDocumentClient.send(
+          new GetCommand({
+            TableName: 'test',
+            Key: { id },
+          }),
+        )
+      ).Item ?? null
+    );
   }
 }
