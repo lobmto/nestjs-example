d0bbd0dfb0d0fb545181399cdd5def2c478a1c34
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-07-04 10:12:10 +0900

    refactor: Add factory of DynamoDBDocumentClient

--- src/dynamo/dynamo.controller.ts
+++ src/dynamo/dynamo.controller.ts
@@ -1,28 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { Public } from 'src/auth/auth.decorator';
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from 'src/env';
 
 @Controller('dynamo')
 export class DynamoController {
-  private client: DynamoDBDocumentClient;
-
   constructor(
     private readonly configService: ConfigService<EnvironmentVariables, true>,
-  ) {
-    const dbClient = new DynamoDBClient({
-      endpoint: this.configService.get('DYNAMO_ENDPOINT'),
-    });
-    this.client = DynamoDBDocumentClient.from(dbClient);
-  }
+    private readonly dbClient: DynamoDBDocumentClient,
+  ) {}
 
   @Get()
   @Public()
   async findAll() {
     // 動作テスト用
-    const res = await this.client.send(
+    const res = await this.dbClient.send(
       new GetCommand({
         TableName: this.configService.get('TABLE_NAME'),
         Key: { id: '1234' },
