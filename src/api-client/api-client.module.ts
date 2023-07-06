4aaeeb46edcef40847e82e30c1a9ff5a86342bf6
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-07-06 17:24:14 +0900

    feat: export ApiClientModule

--- src/api-client/api-client.module.ts
+++ src/api-client/api-client.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RecordsApi } from './generated';
 
-@Module({})
+@Module({
+  providers: [RecordsApi],
+  exports: [RecordsApi],
+})
 export class ApiClientModule {}
