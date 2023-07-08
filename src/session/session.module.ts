d75c848493baee881a35a6092b90160a3772351f
Tomohiro Mitsumoto <t_mitsumoto@yumemi.co.jp>
2023-07-09 00:58:10 +0900

    feat: remove resources about session example

--- src/session/session.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SessionController, SessionService } from './session.controller';
-
-@Module({
-  controllers: [SessionController],
-  providers: [SessionService],
-})
-export class SessionModule {}
